e041cea6dc387d71413bbfe0c3791261c14f7028
Osoro Bironga <fanosoro@gmail.com>
2024-03-15 17:01:19 +0300

    wip: framework

--- harness-cdk/src/lib.rs
+++ harness-cdk/src/lib.rs
@@ -1,17 +1,31 @@
-#[macro_export]
-/// This macro should be called when all functions have been annotated with the #[harness] macro
+#![cfg(feature = "__harness-build")]
+
+use linkme::distributed_slice;
+
+pub use harness_macro;
+
+#[distributed_slice]
+static HARNESS_FUNCTIONS: [(&str, fn(&[u8]) -> wapc_guest::CallResult)];
+
+type HarnessFn = fn(&[u8]) -> wapc_guest::CallResult;
+
+/// This macro should be invoked to initialize the harness code
 macro_rules! harness_init {
     () => {
         #[no_mangle]
         pub fn wapc_init() {
-            wapc_guest::register_function("hello", sample_wapc);
+            for (name, func) in HARNESS_FUNCTIONS {
+                wapc_guest::register_function(name, *func)
+            }
         }
     };
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
+    use crate::HARNESS_FUNCTIONS;
     use candid::{Decode, DecoderConfig, Encode};
+    use wapc_guest;
 
     #[test]
     fn candid_serde() {
--- harness-macro/src/lib.rs
+++ harness-macro/src/lib.rs
@@ -1,40 +1,19 @@
 #![recursion_limit = "128"]
 extern crate proc_macro;
 
-use std::collections::BTreeMap;
-use std::sync::Mutex;
-
-use candid::types::Function as CandidFn;
-use candid::types::Type as CandidTy;
-use candid::Func;
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::{quote, ToTokens};
 use syn::{Error, ItemFn, Signature, Type};
 
-lazy_static::lazy_static! {
-    // Carried over from `candid_derive`
-    // Allows aggregation of the harness methods; which can later be registered to the
-    // `wapc_init` function
-    //  See https://github.com/rust-lang/rust/issues/44034
-    static ref METHODS: Mutex<Option<BTreeMap<String, Function>>> =
-        Mutex::new(Some(BTreeMap::default()));
-}
-
-// `wapc_init` is used by the wapc protocol used in the project.
+// `wapc_init` is reserved by the wapc protocol used in the project.
 const RESERVED_METHODS: [&str; 1] = ["wapc_init"];
-
-// The fields still hold type information, are TokenStream raw strings
-struct Function {
-    args: Vec<String>,
-    rets: Vec<String>,
-}
 
 /// This macro is responsible for generating `harness` compatible implementations.
 ///
 /// It only triggers when the flag `harness_impl` is used
 #[proc_macro_attribute]
-pub fn harness(_: TokenStream, item: TokenStream) -> TokenStream {
+pub fn harness(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match syn::parse::<ItemFn>(item) {
         Ok(func) => {
             if cfg!(not(feature = "__harness_build")) {
@@ -52,7 +31,7 @@ pub fn harness(_: TokenStream, item: TokenStream) -> TokenStream {
                 );
             }
 
-            populate_functions(func).map_or_else(|e| e.to_compile_error().into(), Into::into)
+            create_harness_function(func).map_or_else(|e| e.to_compile_error().into(), Into::into)
         }
         Err(_) => TokenStream::from(
             syn::Error::new(
@@ -65,46 +44,55 @@ pub fn harness(_: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 fn create_harness_function(func: ItemFn) -> syn::Result<TokenStream> {
-    let ident = &func.sig.ident;
-    let harness_fn_name = { Ident::new(&format!("__harness_{ident}"), ident.span()) };
+    let (arg_types, ret_types) = get_args(&func.sig)?;
 
-    let (args, rets) = get_args(&func.sig)?;
+    if ret_types.len() <= 1 {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            &format!("we assume is singular or perhaps empty return type"),
+        ));
+    }
+
+    let arg_vars = arg_types
+        .iter()
+        .enumerate()
+        .map(|(n, _)| Ident::new(&format!("__{n}_harness_var"), Span::call_site()))
+        .collect::<Vec<Ident>>();
+
+    let ident = &func.sig.ident;
+    let base_name = format!("__harness_{ident}");
+    let harness_fn_name = Ident::new(&base_name, ident.span());
+    let harness_fn_name_const = Ident::new(&base_name.to_uppercase(), ident.span());
+    let no_return = ret_types.is_empty();
 
     let harness_function = quote! {
+        // We have this here to allow or discovery of the harness functions
+        #[::linkme::distributed_slice(HARNESS_FUNCTIONS)]
+        static #harness_fn_name_const: fn(&[u8]) -> ::wapc_guest::CallResult = #harness_fn_name;
+
         fn #harness_fn_name(payload: &[u8]) -> ::wapc_guest::CallResult {
-            use candid::{Decode, DecoderConfig};
-
             // TODO: allow attributes to be passed to the DecoderConfig, or pick that up from ic_cdk?
-            let payload = Decode!([DecoderConfig::new()]; &payload, String)?;
+            let (#(#arg_vars,)*) = ::candid::Decode!([::candid::DecoderConfig::new()]; &payload, #(#arg_types),*)?;
 
-            crate::#ident()
-
+            match #no_return {
+                true => {
+                    crate::#ident(#(#arg_vars),*);
+                    Ok(vec![])
+                },
+                false => {
+                    // if return type exists, value is of a singular type or composite type
+                    let bytes =  ::candid::Encode!(crate::#ident(#(#arg_vars),*));
+                    Ok(bytes.to_vec())
+                }
+            }
         }
     };
 
     Ok(TokenStream::from(quote! {
+        // TODO: since were building for harness only, strip the ic_cdk annotations; leave other annotations?
         #func
         #harness_function
     }))
-}
-
-fn populate_functions(func: ItemFn) -> syn::Result<TokenStream> {
-    let (args, rets) = get_args(&func.sig)?;
-
-    let args: Vec<String> = args
-        .iter()
-        .map(|t| format!("{}", t.to_token_stream()))
-        .collect();
-    let rets: Vec<String> = rets
-        .iter()
-        .map(|t| format!("{}", t.to_token_stream()))
-        .collect();
-
-    if let Some(map) = METHODS.lock().unwrap().as_mut() {
-        map.insert(func.sig.ident.to_string(), Function { args, rets });
-    }
-
-    Ok(TokenStream::from(quote! { #func }))
 }
 
 // Carried over from `candid_derive`
