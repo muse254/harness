e32f56ef1f6b60dc0379e0a365f7915a43b9f9f6
Osoro Bironga <fanosoro@gmail.com>
2024-07-27 20:44:50 +0300

    address linter

--- harness-cdk/macro_tests/schema_info.rs
+++ harness-cdk/macro_tests/schema_info.rs
@@ -1,9 +1,8 @@
 #![cfg(feature = "__harness-build")]
-#![allow(clippy::large_stack_frames)]
 
 #[test]
 fn test_schema_info() {
     // should be able to compile retrieve schema info
-    let bin = harness_macros::get_binary__!();
-    assert!(!bin.is_empty());
+    let schema = harness_macros::get_schema!();
+    assert!(!schema.program.is_empty());
 }
